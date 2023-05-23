9f0dd8996391d5a26faf9cb6483001065b141f1e
Sandeep K. Dasari <67505311+sandeepkdasari143@users.noreply.github.com>
2023-05-23 04:28:58 +0000

    Aunth Code has been written, doing middleware

---
+++ src/routers/auth.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { signup, login } = require('../controllers/auth.controller');
+const router = express.Router();
+
+router.route('/signup').post(signup);
+router.route('/login').post(login);
+
+module.exports = router
